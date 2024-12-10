c25a7aeea7c17ff5c7554b83b96c588f11dbadcb
Hamza-P <hamza.siddiqui4747@gmail.com>
2024-12-10 09:20:55 +0500

    .

--- script.js
+++ script.js
@@ -136,7 +136,24 @@ function heroAnimaion(condition){
         bot.children[1].style.display = 'none';
         bot.classList.add('cursor-pointer');
     }
-
-    
-
 }
+
+$(function() {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    });
+  });
+  
